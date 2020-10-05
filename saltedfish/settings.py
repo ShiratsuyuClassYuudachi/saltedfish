56d48410c185ab809460393b5ba753405891ea5d
yuu <shiratsuyuclassyuudachi@protonmail.com>
2020-10-05 09:31:15 +0800

    created user model

--- saltedfish/settings.py
+++ saltedfish/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class User(models.Model):
+    account = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    uuid = models.UUIDField()
+    joinDate = models.TimeField()
+    itemToSell = [models.UUIDField()]
+    soldItem = [models.UUIDField()]
+    boughtItem = [models.UUIDField()]
+    wishList = [models.UUIDField()]
+
 # Create your models here.
