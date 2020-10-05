ab0b7efb817cc7a67b3326417150416290175637
yuu <shiratsuyuclassyuudachi@protonmail.com>
2020-10-05 17:21:03 +0800

    fix user

--- user/models.py
+++ user/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    account = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
+class User(AbstractUser):
     uuid = models.UUIDField()
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=40)
     joinDate = models.TimeField()
     itemToSell = [models.UUIDField()]
     soldItem = [models.UUIDField()]
     boughtItem = [models.UUIDField()]
     wishList = [models.UUIDField()]
+    USERNAME_FIELD = 'email'
 
 # Create your models here.
--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+from django.contrib.auth.admin import User
 import time
 import uuid
 import logging
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 
 
 def index(request):
@@ -11,11 +14,12 @@ def index(request):
 
 
 def register(request):
-    model = User
-    model.account = request.POST['account']
-    model.name = request.POST['name']
-    model.email = request.POST['email']
-    model.password = request.POST['password']
+    model = User()
+    return HttpResponse(request.POST.get('account'), status=201)
+    ''' model.account = request.POST.get('account')
+    model.name = request.POST.get('name')
+    model.email = request.POST.get('email')
+    model.password = request.POST.get('password')
     model.uuid = uuid.uuid1()
     model.joinDate = time.localtime()
     try:
@@ -23,5 +27,13 @@ def register(request):
     except:
         logging.log("Saving error")
         return HttpResponse(status=500)
-    return HttpResponse(status=201)
+    return HttpResponse(request.POST.get('account'), status=201)'''
+
+
+def information(request):
+    if request.user.is_authenticated:
+        return HttpResponse(request.user.last_login)
+    else:
+        return HttpResponse("unauthenticated",status=401)
+
 # Create your views here.
