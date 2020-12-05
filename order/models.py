251df9f2b46a3b10e029b99a5395ec982a55f052
yuu <shiratsuyuclassyuudachi@protonmail.com>
2020-12-05 20:39:51 +0800

    unknown updates

--- order/models.py
+++ order/models.py
@@ -9,6 +9,10 @@ class Order(models.Model):
     value = models.FloatField(),
     time = models.TimeField(),
     check = models.UUIDField(),
+    paid = models.BooleanField(),
+    finished = models.BooleanField(),
+    buyerScore = models.IntegerField(),  # 买家评分
+    sellerScore = models.IntegerField(),  # 卖家评分
     buyerComment = models.CharField(max_length=500),
     sellerComment = models.CharField(max_length=500)
 # Create your models here.
--- order/views.py
+++ order/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from user.models import User
+import time
+import uuid
+import logging
+from django.contrib.auth import authenticate
+import json
+from order.models import Order
+from items.models import Item
 
+
+def buy(request):
+    if request.session.get("uuid") is not None:
+        if User.objects.filter(uuid=uuid.UUID(request.session.get("uuid"))).exists():
+            item = request.POST.get('item')
+            currtime = time.time()
+            value = Item.objects.get(UUID__exact=item).price
+            buyer = uuid
+            seller = Item.objects.get(UUID__exact=item).owner
+
+        return HttpResponse(status=401)
 # Create your views here.
